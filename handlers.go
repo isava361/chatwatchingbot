9b07c17b1ae0eea39429ad12f9e4ecfc1d0548ea
Ivan Savelev <iahito361@gmail.com>
2023-04-23 14:52:02 +0000

    Replace handlers.go

--- handlers.go
+++ handlers.go
@@ -27,7 +27,7 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Conf
 		if len(message.ReplyToMessage.Photo) > 0 {
 			photoFileID = message.ReplyToMessage.Photo[len(message.ReplyToMessage.Photo)-1].FileID
 			newResponse = ""
-			photoFilename, _ = downloadAndSavePhoto(bot, photoFileID, "/home/longspear/chatwatchingbotphotos")
+			photoFilename, _ = downloadAndSavePhoto(bot, photoFileID, "/home/longspear/chatwatchingbot/photos")
 		}
 
 		newMyResponse := MyResponse{
@@ -40,7 +40,7 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Conf
 		config.MyResponses = append(config.MyResponses, newMyResponse)
 
 		// Save the updated config to the JSON file
-		err := saveConfig("/home/longspear/chatwatchingbotconfig/config.json", config)
+		err := saveConfig("config.json", *config)
 		if err != nil {
 			log.Printf("Error saving config: %v", err)
 		}
@@ -54,16 +54,19 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Conf
 	for _, myResponse := range config.MyResponses {
 		if messageContains(receivedMessage, myResponse.SearchPhrase) {
 			if (message.Chat.Type == "supergroup" || message.Chat.Type == "group") && myResponse.PhotoFilename != "" {
-				msg := tgbotapi.NewPhotoUpload(message.Chat.ID, tgbotapi.FileBytes{Name: "photo.jpg", Bytes: nil})
-				msg.FilePath = myResponse.PhotoFilename
+				photoBytes, err := ioutil.ReadFile(myResponse.PhotoFilename)
+				if err != nil {
+					return err
+				}
+				msg := tgbotapi.NewPhotoUpload(message.Chat.ID, tgbotapi.FileBytes{Name: "photo.jpg", Bytes: photoBytes})
 				msg.ReplyToMessageID = message.MessageID
-				_, err := bot.Send(msg)
+				_, err = bot.Send(msg)
 				if err != nil {
 					return err
 				}
 			} else if message.Chat.Type == "supergroup" || message.Chat.Type == "group" {
 				msg := tgbotapi.NewMessage(message.Chat.ID, myResponse.Response)
-				msg.ReplyToMessageID = message.MessageID // Set the ReplyToMessageID field
+				msg.ReplyToMessageID = message.MessageID
 				_, err := bot.Send(msg)
 				if err != nil {
 					return err
@@ -82,162 +85,3 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Conf
 
 	return nil
 }
-
-
-/*
-
-func handleListCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) {
-	allowedUsers := config.AllowedUsers
-	if message.Chat.Type == "private" && allowedUsers[message.From.ID] {
-		var whitelistLines []string
-		for channel := range config.Whitelist {
-			whitelistLines = append(whitelistLines, channel)
-		}
-		whitelistString := strings.Join(whitelistLines, "\n")
-		if whitelistString == "" {
-			whitelistString = "The whitelist is empty."
-		}
-
-		msg := tgbotapi.NewMessage(message.Chat.ID, "Whitelist contents: \n\n"+whitelistString)
-		bot.Send(msg)
-	} else {
-		msg := tgbotapi.NewMessage(message.Chat.ID, "You are not authorized to use this command.")
-		bot.Send(msg)
-	}
-}
-
-func handleListTempChatsCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) {
-	allowedUsers := config.AllowedUsers
-	tempChats := config.TempChats
-
-	if message.Chat.Type == "private" && allowedUsers[message.From.ID] {
-		if len(tempChats) == 0 {
-			msg := tgbotapi.NewMessage(message.Chat.ID, "No temporary chats found.")
-			bot.Send(msg)
-			return
-		}
-
-		tempChatList := ""
-
-		for _, tempChat := range tempChats {
-			tempChatList += fmt.Sprintf("%s - %dm - %v\n", tempChat.ChatName, tempChat.TimeAdded, tempChat.DateTimeAdded.Format("2006-01-02 15:04:05"))
-		}
-
-		msg := tgbotapi.NewMessage(message.Chat.ID, "Temporary added chats:\n\n"+tempChatList)
-		bot.Send(msg)
-
-	} else {
-		msg := tgbotapi.NewMessage(message.Chat.ID, "You are not authorized to use this command.")
-		bot.Send(msg)
-	}
-}
-
-func handleTempAddCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) {
-	if message.Chat.Type == "private" && config.AllowedUsers[message.From.ID] {
-		args := strings.Split(message.Text, " ")
-		if len(args) < 3 {
-			msg := tgbotapi.NewMessage(message.Chat.ID, "Usage: /tempadd @channelusername duration_in_minutes")
-			_, _ = bot.Send(msg)
-			return
-		}
-
-		duration, err := strconv.Atoi(args[2])
-		if err != nil || duration <= 0 || duration > 30 {
-			msg := tgbotapi.NewMessage(message.Chat.ID, "Duration must be an integer between 1 and 30 minutes")
-			_, _ = bot.Send(msg)
-			return
-		}
-
-		channel := args[1]
-
-		if channel != "" {
-			config.Whitelist[channel] = true
-			err := addChannelToWhitelist(config, channel)
-			if err != nil {
-				log.Printf("Error updating whitelist: %v", err)
-			} else {
-				msg := tgbotapi.NewMessage(message.Chat.ID, "Channel temporarily added to whitelist: "+channel)
-				bot.Send(msg)
-
-				// Add temporary chat to config and update JSON
-				addTempChat(config, channel, duration, time.Now())
-			}
-		}
-	} else if !config.AllowedUsers[message.From.ID] {
-		msg := tgbotapi.NewMessage(message.Chat.ID, "You are not authorized to use this command")
-		_, _ = bot.Send(msg)
-		return
-	}
-}
-
-
-
-func handleRemoveCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) {
-	if message.From.ID == config.AllowedUser && message.Chat.Type == "private" {
-		channel := strings.TrimSpace(strings.TrimPrefix(message.Text, "/remove"))
-		if channel != "" {
-			err := removeFromWhitelist(config, channel)
-			if err != nil {
-				log.Printf("Error removing channel from whitelist: %v", err)
-			} else {
-				removeFromWhitelist(config, channel)
-				msg := tgbotapi.NewMessage(message.Chat.ID, "Channel removed from whitelist: "+channel)
-				bot.Send(msg)
-			}
-		}
-	} else if message.Chat.Type != "private" || message.From.ID != config.AllowedUser {
-		msg := tgbotapi.NewMessage(message.Chat.ID, "You are not authorized to use this command")
-		bot.Send(msg)
-	}
-}
-
-func handleWhitelistCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) {
-	allowedUser := config.AllowedUser
-	whitelist := config.Whitelist
-	if message.From.ID == allowedUser && message.Chat.Type == "private" {
-		channel := strings.TrimSpace(strings.TrimPrefix(message.Text, "/whitelist"))
-		if channel != "" {
-			whitelist[channel] = true
-			config.Whitelist = whitelist
-			err := writeConfig("test-config.json", *config)
-			if err != nil {
-				log.Printf("Error updating whitelist: %v", err)
-			} else {
-				msg := tgbotapi.NewMessage(message.Chat.ID, "Channel added to whitelist: "+channel)
-				bot.Send(msg)
-			}
-		}
-	} else if message.Chat.Type != "private" || message.From.ID != allowedUser {
-		msg := tgbotapi.NewMessage(message.Chat.ID, "You are not authorized to use this command")
-		bot.Send(msg)
-	}
-}
-
-func handleEnableBotCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) {
-	allowedUsers := config.AllowedUsers
-	if message.Chat.Type == "supergroup" && allowedUsers[message.From.ID] {
-		err := addChatToAllowedChats(config, message.Chat.ID)
-		if err != nil {
-			log.Printf("Error updating allowed chats: %v", err)
-		} else {
-			msg := tgbotapi.NewMessage(message.Chat.ID, "Bot enabled in this chat.")
-			bot.Send(msg)
-		}
-	}
-}
-
-func handleDisableBotCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) {
-	allowedUsers := config.AllowedUsers
-	allowedChats := config.AllowedChats
-	if message.Chat.Type == "supergroup" && allowedUsers[message.From.ID] {
-		allowedChats[message.Chat.ID] = true
-		err := removeChatFromAllowedChats(config, message.Chat.ID)
-		if err != nil {
-			log.Printf("Error updating allowed chats: %v", err)
-		} else {
-			msg := tgbotapi.NewMessage(message.Chat.ID, "Bot disabled in this chat.")
-			bot.Send(msg)
-		}
-	}
-}
-*/
