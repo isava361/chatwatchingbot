9b3d26becb7b0acf513f84c9506118cce779356e
Ivan Savelev <iahito361@gmail.com>
2023-04-23 13:16:37 +0000

    Replace handlers.go

--- handlers.go
+++ handlers.go
@@ -11,7 +11,7 @@ import (
 func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) error {
 	if message.Text == "test" {
 		msg := tgbotapi.NewMessage(message.Chat.ID, "Ты пидор")
-		_, err = bot.Send(msg)
+		_, err := bot.Send(msg)
 		bot.Send(msg)
 		if err != nil {
 			return err
