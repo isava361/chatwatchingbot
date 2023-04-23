19b50c337288a695ce95b74c6409ec13a3209445
Ivan Savelev <iahito361@gmail.com>
2023-04-23 14:43:28 +0000

    Replace handlers.go

--- handlers.go
+++ handlers.go
@@ -3,8 +3,10 @@
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"strings"
-/*	"log"
-	"time"
+	"path/filepath"
+	"log"
+	"encoding/json"
+/*	"time"
 	"fmt"  */
 )
 
@@ -15,22 +17,66 @@ func messageContains(messageText, targetString string) bool {
 func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Config) error {
 	receivedMessage := message.Text
 
+	// Handle the /add command
+	if message.ReplyToMessage != nil && message.Command() == "add" {
+		newSearchPhrase := strings.TrimSpace(strings.TrimPrefix(receivedMessage, "/add"))
+		newResponse := message.ReplyToMessage.Text
+		photoFileID := ""
+		photoFilename := ""
+
+		if len(message.ReplyToMessage.Photo) > 0 {
+			photoFileID = message.ReplyToMessage.Photo[len(message.ReplyToMessage.Photo)-1].FileID
+			newResponse = ""
+			photoFilename, _ = downloadAndSavePhoto(bot, photoFileID, "/home/longspear/chatwatchingbotphotos")
+		}
+
+		newMyResponse := MyResponse{
+			SearchPhrase:  newSearchPhrase,
+			Response:      newResponse,
+			PhotoFileID:   photoFileID,
+			PhotoFilename: photoFilename,
+		}
+
+		config.MyResponses = append(config.MyResponses, newMyResponse)
+
+		// Save the updated config to the JSON file
+		err := saveConfig("/home/longspear/chatwatchingbotconfig/config.json", config)
+		if err != nil {
+			log.Printf("Error saving config: %v", err)
+		}
+
+		msg := tgbotapi.NewMessage(message.Chat.ID, "New response added!")
+		_, _ = bot.Send(msg)
+
+		return nil
+	}
+
 	for _, myResponse := range config.MyResponses {
-		if messageContains(receivedMessage, myResponse.SearchPhrase) &&  (message.Chat.Type == "supergroup" || message.Chat.Type == "group") {
-			msg := tgbotapi.NewMessage(message.Chat.ID, myResponse.Response)
-			msg.ReplyToMessageID = message.MessageID // Set the ReplyToMessageID field
-			_, err := bot.Send(msg)
-			if err != nil {
-				return err
+		if messageContains(receivedMessage, myResponse.SearchPhrase) {
+			if (message.Chat.Type == "supergroup" || message.Chat.Type == "group") && myResponse.PhotoFilename != "" {
+				msg := tgbotapi.NewPhotoUpload(message.Chat.ID, tgbotapi.FileBytes{Name: "photo.jpg", Bytes: nil})
+				msg.FilePath = myResponse.PhotoFilename
+				msg.ReplyToMessageID = message.MessageID
+				_, err := bot.Send(msg)
+				if err != nil {
+					return err
+				}
+			} else if message.Chat.Type == "supergroup" || message.Chat.Type == "group" {
+				msg := tgbotapi.NewMessage(message.Chat.ID, myResponse.Response)
+				msg.ReplyToMessageID = message.MessageID // Set the ReplyToMessageID field
+				_, err := bot.Send(msg)
+				if err != nil {
+					return err
+				}
+				break
+			} else {
+				msg := tgbotapi.NewMessage(message.Chat.ID, myResponse.Response)
+				_, err := bot.Send(msg)
+				if err != nil {
+					return err
+				}
+				break
 			}
-			break
-		} else if messageContains(receivedMessage, myResponse.SearchPhrase){
-			msg := tgbotapi.NewMessage(message.Chat.ID, myResponse.Response)
-			_, err := bot.Send(msg)
-			if err != nil {
-				return err
-			}
-			break
 		}
 	}
 
