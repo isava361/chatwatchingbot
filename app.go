3e55ff3325a23eaae112a4eabb92407ee86ee876
longspear <longspear@vds2345095.my-ihor.ru>
2023-04-25 02:20:35 +0300

    updated formating app.go

--- app.go
+++ app.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"github.com/fsnotify/fsnotify"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
-	"github.com/fsnotify/fsnotify"
 )
 
 type FileType string
+
 const FilePhoto FileType = "photo"
 const FileGIF FileType = "gif"
 const FileSticker FileType = "sticker"
 const FileVoice FileType = "voice"
 
 type MyResponse struct {
-	SearchPhrase      string    `json:"searchPhrase"`
-	Response          string    `json:"response,omitempty"`
-	FileType          FileType    `json:"fileType,omitempty"`
-	FileID            string    `json:"fileID,omitempty"`
-	FileName          string    `json:"filename,omitempty"`
+	SearchPhrase string   `json:"searchPhrase"`
+	Response     string   `json:"response,omitempty"`
+	FileType     FileType `json:"fileType,omitempty"`
+	FileID       string   `json:"fileID,omitempty"`
+	FileName     string   `json:"filename,omitempty"`
 }
 
 type Config struct {
-	MyResponses    []MyResponse      `json:"myResponses"`
-	ChatTriggers map[int64][]MyResponse  `json:"chat_triggers,omitempty"`
+	MyResponses  []MyResponse           `json:"myResponses"`
+	ChatTriggers map[int64][]MyResponse `json:"chat_triggers,omitempty"`
 }
 
 const configlocation = "./config/config.json"
@@ -32,7 +33,6 @@ func main() {
 	if err != nil {
 		log.Panicf("Config error: %v", err)
 	}
-
 
 	token, err := readBotToken("./config/token.txt")
 	if err != nil {
@@ -48,10 +48,7 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-
 	updates := bot.GetUpdatesChan(u)
-
-	
 
 	// Add the configuration file to the watcher
 	err = watcher.Add(configlocation)
@@ -77,8 +74,8 @@ func main() {
 						log.Printf("Error reading config: %v", err)
 					}
 				}
-				case err := <-watcher.Errors:
-					log.Println("Error in file watcher:", err)
+			case err := <-watcher.Errors:
+				log.Println("Error in file watcher:", err)
 			}
 		}
 	}()
