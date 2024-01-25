ccb52955bbcef2533d6f2559f8de980936313974
Ivan Savelev <iahito361@gmail.com>
2024-01-25 20:39:44 +0000

    Update file handlers.go

--- handlers.go
+++ handlers.go
@@ -349,7 +349,7 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, config *Conf
 
 	currentTime,_ := getCurrentTimeForLocation("America/Los Angeles")
 
-	if message.Chat.ID == -1001245934322 && messageContains(receivedMessage, "@Porky8888")/* && isTimeBetween2AMAnd7AM (currentTime)*/ {
+	if message.Chat.ID == -1001245934322 && messageContains(receivedMessage, "@Porky8888") && isTimeBetween2AMAnd7AM (currentTime) {
 		photoMsg := tgbotapi.NewPhoto(message.Chat.ID, tgbotapi.FileID("AgACAgQAAx0Cc2pGjQACAUBlssL7rSKP4mmzMMYeORKjAS3LOAACHMIxGzznmFF5Spk5RRTfbwEAAwIAA3gAAzQE"))
 		photoMsg.ReplyToMessageID = message.MessageID
 		photoMsg.Caption = fmt.Sprintf("Машталер в %v утра", currentTime.Hour())
