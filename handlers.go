9148eee29cb65b492c9d14c5e64ad191b5ad0762
Ivan Savelev <iahito361@gmail.com>
2024-08-30 06:13:01 +0000

    Update file handlers.go

--- handlers.go
+++ handlers.go
@@ -482,9 +482,9 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, db *sql.DB) 
 		photoMsg := tgbotapi.NewPhoto(message.Chat.ID, tgbotapi.FileID(fileID))
 		photoMsg.ReplyToMessageID = message.MessageID
         if isTimeBetween(currentTime, 2, 4) {
-			photoMsg.Caption = fmt.Sprintf("Кел в %v ночи", currentTime.Hour())
+			photoMsg.Caption = fmt.Sprintf("Кел в %v ночи", currentTimeNewYork.Hour())
 		} else {
-			photoMsg.Caption = fmt.Sprintf("Кел в %v утра", currentTime.Hour())
+			photoMsg.Caption = fmt.Sprintf("Кел в %v утра", currentTimeNewYork.Hour())
 		}
         bot.Send(photoMsg)
 		return nil
