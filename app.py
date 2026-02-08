9dac9767e0b34957c499155c2b0acbbe93c3afec
isava361 <34292420+isava361@users.noreply.github.com>
2026-02-08 04:34:35 +0300

    Use UUID for Jitsi room name

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def _is_allowed_user(user_id: int, data: Dict[str, Any]) -> bool:
 
 
 def generate_jitsi_link(user_name: str, user_id: str, app_secret: str) -> str:
-    room_name = "*"
+    room_name = str(uuid.uuid4())
     now = int(time.time())
     payload = {
         "aud": "jitsi",
